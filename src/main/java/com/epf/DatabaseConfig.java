7b7681822cc69d2d1111730422e7537fb5f204db
Greg <gregoire.brun@epfedu.fr>
2025-02-24 15:09:00 +0100

    Ajout de la databases config

--- src/main/java/com/epf/DatabaseConfig.java
+++ src/main/java/com/epf/DatabaseConfig.java
@@ -6,5 +6,18 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import javax.sql.DataSource;
 @Configuration
 public class DatabaseConfig {
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/pvz?useSSL=false&serverTimezone=UTC");
+        dataSource.setUsername("epf");
+        dataSource.setPassword("MotDePasse123!");
+        return dataSource;
+    }
 
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
 }
---
+++ src/main/java/com/epf/Plante.java
@@ -0,0 +1,4 @@
+package com.epf;
+
+public class Plante {
+}
