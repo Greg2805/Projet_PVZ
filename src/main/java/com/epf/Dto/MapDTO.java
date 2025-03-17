c4b061853a7e2647753c8b8ffcc15800446ca713
Greg <gregoire.brun@epfedu.fr>
2025-03-17 14:44:31 +0100

    fait tous les dto

--- src/main/java/com/epf/Dto/MapDTO.java
+++ src/main/java/com/epf/Dto/MapDTO.java
@@ -1,4 +1,25 @@
 package com.epf.Dto;
 
 public class MapDTO {
+    private int nbLigne;
+    private int nbColonne;
+
+    public MapDTO(int nbLigne, int nbColonne) {
+        this.nbLigne = nbLigne;
+        this.nbColonne = nbColonne;
+    }
+    public MapDTO() {
+    }
+    public int getNbLigne() {
+        return nbLigne;
+    }
+    public void setNbLigne(int nbLigne) {
+        this.nbLigne = nbLigne;
+    }
+    public int getNbColonne() {
+        return nbColonne;
+    }
+    public void setNbColonne(int nbColonne) {
+        this.nbColonne = nbColonne;
+    }
 }
--- src/main/java/com/epf/Dto/PlanteDTO.java
+++ src/main/java/com/epf/Dto/PlanteDTO.java
@@ -1,4 +1,40 @@
 package com.epf.Dto;
 
 public class PlanteDTO {
+    private String nom;
+    private int pointDeVie;
+    private int degat_attaque;
+
+    public PlanteDTO(String nom, int pointDeVie, int degat_attaque) {
+        this.nom = nom;
+        this.pointDeVie = pointDeVie;
+        this.degat_attaque = degat_attaque;
+    }
+
+    public PlanteDTO() {
+    }
+
+    public String getNom() {
+        return nom;
+    }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    public int getPointDeVie() {
+        return pointDeVie;
+    }
+
+    public void setPointDeVie(int pointDeVie) {
+        this.pointDeVie = pointDeVie;
+    }
+    public int getDegat_attaque() {
+        return degat_attaque;
+    }
+    public void setDegat_attaque(int degat_attaque) {
+        this.degat_attaque = degat_attaque;
+    }
+
+
+
+
 }
